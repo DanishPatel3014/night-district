dee3b046a5131140e01fa338a1f697f3ce2ba44e
Danish Patel <93546025+DanishPatel3014@users.noreply.github.com>
2023-01-11 18:33:03 +0500

    updates 1-11-23

--- src/views/Website/Home.js
+++ src/views/Website/Home.js
@@ -1,22 +1,12 @@
 import { Stack, Text } from "@chakra-ui/react";
 import Header from '../../components/Website/Header'
+import HomeBanner from "../../components/Website/Banners/HomeBanner";
 export default function Home() {
 
   return (
-    <Stack
-      bgColor={'#1a1a1a'}
-      w={'100%'}
-      h={'100vh'}
-      direction={'row'}
-      alignItems={'center'}
-      justifyContent={'center'}
-      color={'#fff'}  
-      >
-        <Text
-          fontSize={38}
-        >
-         
-          Let's Start Something Awesome!</Text>
+    <Stack>
+<HomeBanner/>
+      
     </Stack>
   );
 }
